81a5aae993847bd67e4e8621f5cba94419340f93
Walter McGinnis <wm@waltermcginnis.com>
2019-06-14 11:53:52 +1200

    Remove unnecessary import

--- lib/extraction_point/content_type_field_mapping.ex
+++ lib/extraction_point/content_type_field_mapping.ex
@@ -1,8 +1,6 @@
 defmodule ExtractionPoint.ContentTypeFieldMapping do
   use Ecto.Schema
 
-  import Ecto.Query
-
   schema "content_type_to_field_mappings" do
     belongs_to :content_type, ExtractionPoint.ContentType
     belongs_to :extended_field, ExtractionPoint.ExtendedField
