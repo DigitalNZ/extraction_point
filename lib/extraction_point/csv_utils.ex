ac2c76e0a08e34809b1d40115fb2e3809cb2dcf4
Walter McGinnis <wm@waltermcginnis.com>
2019-07-06 17:00:17 +1200

    Strip elixir specific datetime formating

--- lib/extraction_point/csv_utils.ex
+++ lib/extraction_point/csv_utils.ex
@@ -2,6 +2,7 @@ defmodule ExtractionPoint.CSVUtils do
   def values_to_stringables(row) do
     row |> Enum.map(fn v ->
       case v do
+        %NaiveDateTime{} -> v |> NaiveDateTime.truncate(:second) |> NaiveDateTime.to_string()
         value when is_map(value) or is_list(value) -> value |> inspect()
         _ -> v
       end
