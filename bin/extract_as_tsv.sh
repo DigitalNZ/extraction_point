8ca07266a1939edfa22e069cddee63592c81afbc
Walter McGinnis <wm@waltermcginnis.com>
2019-07-06 16:08:34 +1200

    Add wrapper scripts to simplify CLI for extracting data

---
+++ bin/extract_as_tsv.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+TYPE=$1
+OUTPUT=$2
+options=$3
+
+command_root="docker-compose run requestor curl -H"
+command="$command_root \"Accept: text/csv\" -X GET \"http://app:4000/"
+
+case $TYPE in
+    audio-recordings | documents | still-images | vidoes | web-links) command="$command$TYPE.csv";;
+    *) topic_stub="topics.csv?topic_type=$TYPE"; command="$command$topic_stub";;
+esac
+
+if [[ -n "${options// /}" ]]; then
+    echo "added $options to request"
+
+    case "${command: -3}" in
+        csv) options="?$options";;
+        *) options="&$options";;
+    esac
+
+    command="$command$options\""
+else
+    command="$command\""
+fi
+
+if [[ -n "${OUTPUT// /}" ]]; then
+    command="$command --output $OUTPUT"
+else
+    command="$command --output $TYPE.csv"
+fi
+
+echo $command
+
+eval $command
