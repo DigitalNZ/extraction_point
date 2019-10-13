79b5266e551820caf3cbe63dbc691dcfe49483b8
Walter McGinnis <wm@waltermcginnis.com>
2019-10-13 14:56:56 +1300

    Add label cleaning to pluralize as well as single label
    Also takes out trailing spaces that may be result of parens clean up.

--- lib/extraction_point/extended_field/label_key.ex
+++ lib/extraction_point/extended_field/label_key.ex
@@ -1,13 +1,21 @@
 defmodule ExtractionPoint.ExtendedField.LabelKey do
   def to_key(label) do
     label
-    |> String.replace("(", " ")
-    |> String.replace(")", " ")
+    |> clean()
     |> Inflex.parameterize("_")
   end
+
   def to_plural_key(label) do
     label
+    |> clean()
     |> Inflex.pluralize()
     |> Inflex.parameterize("_")
+  end
+
+  defp clean(label) do
+    label
+    |> String.replace("(", " ")
+    |> String.replace(")", " ")
+    |> String.trim()
   end
 end
