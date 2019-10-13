ab453686ef54272516e6d1f159c818618704ccf4
Walter McGinnis <wm@waltermcginnis.com>
2019-10-13 13:02:35 +1300

    Remover parenthesis from labels

--- lib/extraction_point/extended_field/label_key.ex
+++ lib/extraction_point/extended_field/label_key.ex
@@ -1,4 +1,13 @@
 defmodule ExtractionPoint.ExtendedField.LabelKey do
-  def to_key(label), do: label |> Inflex.parameterize("_")
-  def to_plural_key(label), do: label |> Inflex.pluralize() |> Inflex.parameterize("_")
+  def to_key(label) do
+    label
+    |> String.replace("(", " ")
+    |> String.replace(")", " ")
+    |> Inflex.parameterize("_")
+  end
+  def to_plural_key(label) do
+    label
+    |> Inflex.pluralize()
+    |> Inflex.parameterize("_")
+  end
 end
