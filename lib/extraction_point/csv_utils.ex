d9bd038371798ae3d7029d9560aa707c648e7b80
Walter McGinnis <wm@waltermcginnis.com>
2019-12-18 10:39:20 +1300

    Cast charlists to list represenation
    We never want charlists in this context.

--- lib/extraction_point/csv_utils.ex
+++ lib/extraction_point/csv_utils.ex
@@ -3,7 +3,7 @@ defmodule ExtractionPoint.CSVUtils do
     row |> Enum.map(fn v ->
       case v do
         %NaiveDateTime{} -> v |> NaiveDateTime.truncate(:second) |> NaiveDateTime.to_string()
-        value when is_map(value) or is_list(value) -> value |> inspect()
+        value when is_map(value) or is_list(value) -> value |> inspect([charlists: :as_lists])
         _ -> v
       end
     end)
