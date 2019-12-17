39a6d110962c2802ccce87f24e69148f98fcbda7
Walter McGinnis <wm@waltermcginnis.com>
2019-12-18 11:52:24 +1300

    Add listing related ids & titles of source topics
    This lists from  the other side of relationship. E.g. a still_image
    can list the topics it is in.
    
    Refactor previous listing of related ids and types for containing topic.

--- lib/extraction_point/exporter.ex
+++ lib/extraction_point/exporter.ex
@@ -91,24 +91,56 @@ defmodule ExtractionPoint.Exporter do
     "#{select_all_from(type)} ORDER BY id"
   end
 
+  # returns ids and titles columns for each type of SOURCE topic
+  # that this row is a related_item for in extracted_relations
+  # can be called on either on topic types
+  # or content itemt types (e.g. still_image)
+  defp raw_sql(type, %{"include_related" => "source"} = options) do
+    type_as_table = to_table_name(type)
+    type_table_prefix = "target"
+
+    parts = related_source_tables(type_as_table)
+    |> Enum.with_index()
+    |> Enum.reduce(%{selects: [], joins: []}, fn related_meta, acc ->
+      {topic_table, index} = related_meta
+
+      {ids_column, titles_column} = ids_titles_column_names(topic_table)
+
+      agg_args = %{ids_and_titles_subquery: source_topics_ids_and_titles_subquery(topic_table, index, type_as_table),
+                   index: index,
+                   type_as_table: type_as_table,
+                   ids_column: ids_column,
+                   titles_column: titles_column,
+                   join_column: "related_item_id"}
+
+      aggsubt = "aggsubt#{index}"
+
+      select_clause = select_clause(aggsubt, ids_column, titles_column)
+
+      join_clause = join_clause(aggsubt, aggregate_subquery(agg_args), type_table_prefix)
+
+      %{selects: acc.selects ++ [select_clause], joins: acc.joins ++ [join_clause]}
+    end)
+
+    related_sql_from_parts(options, type_as_table, type_table_prefix, parts)
+  end
+
+  # returns ids and titles columns
+  # for each type of RELATED ITEM (can be topic, still_image, web_link, etc.)
+  # that this row is SOURCE topic for in extracted_relations
+  # can be called only on topic types (as only topics are SOURCE in relations)
   defp raw_sql(type, %{"include_related" => include_related} = options) when not is_nil(include_related) do
     type_as_table = to_table_name(type)
     type_table_prefix = "target"
 
-    # create map of tables to prefixes
-    # create relations subquery name prefix for each iteration
-    # accumulate query parts in lists per related table
-    # select has ids and titles columns per related table
-    # and target all columns wildcard once
     parts = related_tables(type_as_table)
     |> Enum.with_index()
     |> Enum.reduce(%{selects: [], joins: []}, fn related_meta, acc ->
       {related_table, index} = related_meta
 
-      ids_column = "#{related_table}_ids"
-      titles_column = "#{related_table}_titles"
+      {ids_column, titles_column} = ids_titles_column_names(related_table)
 
-      agg_args = %{related_ids_and_titles_subquery: related_ids_and_titles_subquery(related_table, index, type_as_table),
+      agg_args = %{ids_and_titles_subquery: related_ids_and_titles_subquery(related_table, index, type_as_table),
                    index: index,
                    type_as_table: type_as_table,
                    ids_column: ids_column,
@@ -116,41 +148,14 @@ defmodule ExtractionPoint.Exporter do
 
       aggsubt = "aggsubt#{index}"
 
-      select_clause = ~s"""
-      #{aggsubt}.#{ids_column},
-      #{aggsubt}.#{titles_column}
-      """
+      select_clause = select_clause(aggsubt, ids_column, titles_column)
 
-      join_clause = ~s"""
-      FULL JOIN (#{aggregate_subqquery(agg_args)}) #{aggsubt}
-      ON #{type_table_prefix}.id = #{aggsubt}.id
-      """
+      join_clause = join_clause(aggsubt, aggregate_subquery(agg_args), type_table_prefix)
 
       %{selects: acc.selects ++ [select_clause], joins: acc.joins ++ [join_clause]}
     end)
 
-    sql = ~s"""
-    SELECT #{type_table_prefix}.*,
-    #{Enum.join(parts.selects, ",\n")}
-    FROM #{type_as_table} #{type_table_prefix}
-    #{Enum.join(parts.joins, "\n")}
-    """
-
-    # add basket where clause if necessary
-    sql = case build_baskets_where_clause("#{type_table_prefix}.", options) do
-            nil -> sql
-            value -> "#{sql} WHERE #{value}"
-          end
-
-    sql = "#{sql} ORDER BY #{type_table_prefix}.id"
-
-    sql = sql
-    |> add_to_sql_if_present(options, "limit")
-    |> add_to_sql_if_present(options, "offset")
-
-    File.write("full.sql", sql)
-
-    sql
+    related_sql_from_parts(options, type_as_table, type_table_prefix, parts)
   end
 
   defp raw_sql(type, %{"except_baskets" => _} = options), do: raw_basket_sql(type, options)
@@ -195,6 +200,15 @@ defmodule ExtractionPoint.Exporter do
     ) |> Repo.all()
   end
 
+  defp related_source_tables(type_as_table) do
+    from(
+      rt in "extracted_relations",
+      distinct: true,
+      select: rt.source_table,
+      where: rt.related_item_table == ^type_as_table
+    ) |> Repo.all()
+  end
+
   defp related_ids_and_titles_subquery(related_table, index, type_as_table) do
     rit = "ri#{index}"
     relst = "r#{index}"
@@ -210,18 +224,71 @@ defmodule ExtractionPoint.Exporter do
     """
   end
 
-  defp aggregate_subqquery(args) do
+  defp source_topics_ids_and_titles_subquery(topic_table, index, type_as_table) do
+    rit = "ri#{index}"
+    relst = "r#{index}"
+
+    ~s"""
+    SELECT #{rit}.id, #{rit}.title, #{relst}.related_item_id
+    FROM #{topic_table} #{rit}
+    JOIN extracted_relations #{relst}
+    ON #{relst}.source_id = #{rit}.id
+    WHERE #{relst}.related_item_table = '#{type_as_table}'
+    AND source_table = '#{topic_table}'
+    AND #{rit}.title != '#{@not_public_title}'
+    """
+  end
+
+  defp aggregate_subquery(args) do
     aggt = "aggt#{args.index}"
     risubt = "risubt#{args.index}"
+    join_column = args[:join_column] || "source_id"
 
     ~s"""
     SELECT #{aggt}.id,
     ARRAY_AGG(#{risubt}.id) AS #{args.ids_column},
     ARRAY_AGG(#{risubt}.title) AS #{args.titles_column}
     FROM #{args.type_as_table} #{aggt}
-    JOIN (#{args.related_ids_and_titles_subquery}) #{risubt}
-    ON #{risubt}.source_id = #{aggt}.id
+    JOIN (#{args.ids_and_titles_subquery}) #{risubt}
+    ON #{risubt}.#{join_column} = #{aggt}.id
     GROUP BY #{aggt}.id
     """
+  end
+
+  defp select_clause(aggsubt, ids_column, titles_column) do
+    ~s"""
+    #{aggsubt}.#{ids_column},
+    #{aggsubt}.#{titles_column}
+    """
+  end
+
+  defp join_clause(aggsubt, subquery, type_table_prefix) do
+    ~s"""
+    FULL JOIN (#{subquery}) #{aggsubt}
+    ON #{type_table_prefix}.id = #{aggsubt}.id
+    """
+  end
+
+  defp ids_titles_column_names(table) do
+    {"#{table}_ids", "#{table}_titles"}
+  end
+
+  defp related_sql_from_parts(options, type_as_table, type_table_prefix, parts) do
+    sql = ~s"""
+    SELECT #{type_table_prefix}.*,
+    #{Enum.join(parts.selects, ",\n")}
+    FROM #{type_as_table} #{type_table_prefix}
+    #{Enum.join(parts.joins, "\n")}
+    """
+
+    # add basket where clause if necessary
+    sql = case build_baskets_where_clause("#{type_table_prefix}.", options) do
+            nil -> sql
+            value -> "#{sql} WHERE #{value}"
+          end
+
+    "#{sql} ORDER BY #{type_table_prefix}.id"
+    |> add_to_sql_if_present(options, "limit")
+    |> add_to_sql_if_present(options, "offset")
   end
 end
